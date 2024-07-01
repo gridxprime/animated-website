92676322ebf68a381fa4e4284e3cd5fa17f134a7
Aibek Everon <ender.kraya@gmail.com>
2024-07-01 22:01:16 +0600

    v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import gsap from 'gsap'
 import  ScrollTrigger  from 'gsap/ScrollTrigger'
 import dedsec from './assets/img/logo1.jpg'
 
+import infCube1 from './assets/img/inf/infcub1.jpg'
+import infCube2 from './assets/img/inf/infcub2.webp'
+import infCube3 from './assets/img/inf/infcub3.jpg'
+
 function App() {
 
   const sectionTitle = useRef(null)
@@ -115,9 +119,9 @@ const btnCubes = () => {
 
 
   gsap.fromTo('.cube2', {
-    x: '200%', delay: .2, rotation: 0
+    x: '200%', delay: .2, rotation: 720
   }, {
-    x: 0, ease: 'bounce.in', duration: 1, rotation: 0
+    x: 0, ease: 'bounce.in', duration: 1.5, rotation: 0
   })
 
 
@@ -128,6 +132,36 @@ const btnCubes = () => {
   }) , []
   
 }
+
+const spanRef = useRef(null)
+
+useEffect(() => {
+
+  gsap.set(spanRef.current , { transformOrigin: 'top left' });
+
+
+  const tl = gsap.timeline({repeat: -1, yoyo: true})
+  tl.to('.inf__span',{ delay: 2,  rotate: 5 , duration: 2, ease: 'back.in' })
+})
+
+useEffect(() => {
+ const infCube =  gsap.timeline({repeat: -1, yoyo: true})
+  infCube.to('.inf__cube1', {
+    scale: .8, duration: 1, delay: .5, borderRadius: '50%', ease: 'bounce.inOut'
+  })
+
+  const inf2Cube = gsap.timeline({repeat: -1, yoyo: true})
+  inf2Cube.to('.inf__cube2', {
+    scale: .3, rotation: 180, delay: .4, duration: 1.2, ease: 'back.inOut'
+  }) , []
+
+  const inf3Cube = gsap.timeline({repeat: -1, yoyo: true})
+  inf3Cube.to('.inf__cube3', {
+    rotate: 5
+  })
+
+})
+
 
   return (
     <>
@@ -147,7 +181,7 @@ const btnCubes = () => {
           <span>Using ScrollTrigger to make these perfect moves!</span>
           <div ref={box1}  className="box1"><img src={dedsec} alt="" /></div>
           <div ref={box2} className="box2"><img src={dedsec} alt="" /></div>
-          <div ref={box3} className="box2"><img src={dedsec} alt="" /></div>
+          <div ref={box3} className="box3"><img src={dedsec} alt="" /></div>
     
         </div>
       </section>
@@ -158,6 +192,17 @@ const btnCubes = () => {
                 <div className="cube3 btn__cube"><span>Forge ahead with courage, for greatness awaits beyond the cube.</span></div>
               </div>
           <button onClick={btnCubes} className='btn__section-btn'>Tap</button>
+      </section>
+      <section className='infinite'>
+        <span ref={spanRef} className='inf__span'>
+          You can use infinite animations via <code>gsap.timeline()</code>
+        </span>
+        <div className="infinite__cubes">
+              <div className="inf__cube1 inf__cube"><img src={infCube1} alt="" /></div>
+              <div className="inf__cube2 inf__cube"><img src={infCube2} alt="" /></div>
+              <div className="inf__cube3 inf__cube"><img src={infCube3} alt="" /></div>
+              
+        </div>
       </section>
     </>
   )
